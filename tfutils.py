3ef3dd56346ad1917d70d0738dfaa6b96fe38d35
gera <gera@satellogic.com>
2017-02-12 23:22:36 -0300

    Factored out some code, AND subgraphs are not created on -p. This doesn't work too well with our graphs

--- tfutils.py
+++ tfutils.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 from IPython.display import clear_output, Image, display, HTML
 
-def show_graph(graph_def):
+def graph_as_HTML(graph_def, baseURL=''):
     # Helper functions for TF Graph visualization
     def _strip_consts(graph_def, max_const_size=32):
         """Strip large constant values from graph_def."""
@@ -38,21 +38,41 @@ def show_graph(graph_def):
                 document.getElementById("{id}").pbtxt = {data};
               }}
             </script>
-            <link rel="import" href="/files/tf-graph/tf-graph-basic.build.html" onload=load()>
+            <link rel="import" href="{baseURL}/files/tf-graph/tf-graph-basic.build.html" onload=load()>
             <div style="height:600px">
               <tf-graph-basic id="{id}"></tf-graph-basic>
             </div>
-        """.format(data=repr(str(strip_def)), id='graph' + str(np.random.rand()))
+        """.format(data=repr(str(strip_def)), id='graph' + str(np.random.rand()), baseURL=baseURL)
 
         iframe = """
             <iframe seamless style="width:800px;height:620px;border:0" srcdoc="{}"></iframe>
         """.format(code.replace('"', '&quot;'))
-        display(HTML(iframe))
+
+        return iframe
+
     # Visualizing the network graph. Be sure expand the "mixed" nodes to see their
     # internal structure. We are going to visualize "Conv2D" nodes.
     if hasattr(graph_def, 'as_graph_def'):
         graph_def = graph_def.as_graph_def()
 
-    tmp_def = _rename_nodes(graph_def, lambda s: "/".join(s.split('_', 1)))
-    _show_entire_graph(tmp_def)
+    tmp_def = graph_def
+    # tmp_def = _rename_nodes(graph_def, lambda s: "/".join(s.split('.', 1)))
+    return _show_entire_graph(tmp_def)
+
+def show_graph(graph_def):
+    iframe = graph_as_HTML(graph_def)
+    display(HTML(iframe))
+
+def writeAsHTML(graphdefFile, htmlFile):
+    graph_def = tf.GraphDef()
+
+    proto_b = open(graphdefFile, "rb").read()
+    graph_def.ParseFromString(proto_b)
+
+    html = graph_as_HTML(graph_def, baseURL='http://localhost:8889')
+    open(htmlFile, 'w').write(html)
+
+if __name__ == '__main__':
+    import sys
+    writeAsHTML(sys.argv[1], sys.argv[2])
 
